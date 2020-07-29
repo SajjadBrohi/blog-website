cacf7c74c781ceaca53addfacd47038420428a1b
SajjadBrohi <s.brohi@uq.net.au>
2020-07-29 15:40:27 +1000

    Server set up

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.render('home');
+});
+
+app.listen(3000, () => console.log('Server started on port 3000'));
+
 // const express = require("express");
 // const bodyParser = require("body-parser");
 // const ejs = require("ejs");
