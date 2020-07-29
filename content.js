905d6ffe67394d01547cb05b62338888d52333e0
SajjadBrohi <s.brohi@uq.net.au>
2020-07-29 20:11:22 +1000

    Implement blog composing

---
+++ content.js
@@ -0,0 +1,41 @@
+exports.homeStartingContent = () => {
+  return 'All your posts will show up here in the home page. \
+  Lacus vel facilisis volutpat est velit egestas dui id ornare. \
+  Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum \
+  sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum\
+  leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales\
+  ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis \
+  parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing \
+  elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor \
+  eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed \
+  vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque \
+  adipiscing.';
+}
+
+exports.aboutContent = () => {
+  return 'My name is Brohi, I create blog posts on web development, \
+  Lacus vel facilisis volutpat est velit egestas dui id ornare. \
+  Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum \
+  sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum\
+  leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales\
+  ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis \
+  parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing \
+  elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor \
+  eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed \
+  vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque \
+  adipiscing.';
+}
+
+exports.contactContent = () => {
+  return ' You can contact me as mentioned in the description below: \
+  Lacus vel facilisis volutpat est velit egestas dui id ornare. \
+  Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum \
+  sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum\
+  leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales\
+  ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis \
+  parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing \
+  elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor \
+  eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed \
+  vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque \
+  adipiscing.';
+}
